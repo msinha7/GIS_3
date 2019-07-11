ed73dca4840054c248de529b71b5394666bff80a
msinha7 <38064755+msinha7@users.noreply.github.com>
2019-07-11 13:34:05 -0400

    Add files via upload

---
+++ Debugging/Solution/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Solution.Repos;
+using Solution.Repos.Interfaces;
+using Solution.Services;
+using Solution.Services.Interfaces;
+
+namespace Solution
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            ITransitionRepository transitions = new TransitionRepository();
+            IStatusRepository statuses = new StatusRepository();
+            ISubtypeRepository subtypes = new SubtypeRepository();
+
+            IStatusService service = new StatusService(transitions, statuses, subtypes);
+
+            var initialDefaultStatuses = service.GetInitialStatuses(SubtypeRepository.DefaultSubtype);
+            var initialValidationStatuses = service.GetInitialStatuses(SubtypeRepository.ValidationSubtype);
+
+            Console.WriteLine("------ Initial Default Statuses ------");
+
+            foreach (var defaultStatus in initialDefaultStatuses)
+                Console.WriteLine(defaultStatus);
+
+            Console.WriteLine("------ Initial Validation Statuses ------");
+
+            foreach (var validationStatus in initialValidationStatuses)
+                Console.WriteLine(validationStatus);
+
+            Console.ReadLine();
+        }
+    }
+}
