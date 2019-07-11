5e34b08d727b4b2d4af12849c1ed5fe62351d77c
msinha7 <38064755+msinha7@users.noreply.github.com>
2019-07-11 13:15:22 -0400

    Add files via upload

---
+++ Debugging/Solution/Interfaces/IStatusService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Solution.Services.Interfaces
+{
+    public interface IStatusService
+    {
+        /// <summary>
+        /// Returns a list of "Initial" <see cref="Model.Status"/> objects that are filtered by the passed in subtype.
+        /// </summary>
+        /// <param name="subtype">The <see cref="Model.Subtype"/> that filters the Statuses returned</param>
+        /// <returns>A list of initial <see cref="Model.Status"/> objects for the passed in <see cref="Model.Subtype"/></returns>
+        IEnumerable<Model.Status> GetInitialStatuses(Model.Subtype subtype);
+    }
+}
