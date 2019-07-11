838c561ba91129bce5958d073460ca82383d2209
msinha7 <38064755+msinha7@users.noreply.github.com>
2019-07-11 01:33:23 -0400

    Add files via upload

---
+++ Debugging/Solution/Repos/StatusRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Solution.Repos.Interfaces;
+
+namespace Solution.Repos
+{
+    /// <summary>
+    /// Implementation of <see cref="IStatusRepository"/> that creates new data to return each time.
+    /// </summary>
+    public class StatusRepository : IStatusRepository
+    {
+        public static readonly Model.Status Open = new Model.Status(0, "Open");
+        public static readonly Model.Status InProgress = new Model.Status(1, "In Progress");
+        public static readonly Model.Status Closed = new Model.Status(2, "Closed");
+        public static readonly Model.Status Validate = new Model.Status(3, "Validate");
+        public static readonly Model.Status ValidationSuccessful = new Model.Status(4, "Validation Successful");
+        public static readonly Model.Status ValidationFailed = new Model.Status(5, "Validation Failed");
+
+        public IEnumerable<Model.Status> GetAll()
+        {
+            return new List<Model.Status> { Open, InProgress, Closed, Validate, ValidationSuccessful, ValidationFailed };
+        }
+    }
+}
---
+++ Debugging/Solution/Repos/SubtypeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Solution.Repos.Interfaces;
+
+namespace Solution.Repos
+{
+    /// <summary>
+    /// Implementation of <see cref="ISubtypeRepository"/> that creates new data to return each time.
+    /// </summary>
+    public class SubtypeRepository : ISubtypeRepository
+    {
+        public static readonly Model.Subtype DefaultSubtype = new Model.Subtype(0, "Default");
+        public static readonly Model.Subtype ValidationSubtype = new Model.Subtype(1, "Validation");
+
+        public IEnumerable<Model.Subtype> GetAll()
+        {
+            return new List<Model.Subtype> { DefaultSubtype, ValidationSubtype };
+        }
+    }
+}
